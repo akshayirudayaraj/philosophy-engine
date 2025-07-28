94c7451434dd182dfcfabf099da2ca8eaebbab2c
akshayirudayaraj <akshay.irudayaraj@gmail.com>
2025-07-27 19:47:55 -0700

    compact article metadata into reusable metadata data template

--- sep/shared_types.py
+++ sep/shared_types.py
@@ -6,6 +6,7 @@ class Section(TypedDict):
   
 class Metadata(TypedDict):
   organization: str
+  intro: str # either a preamble for SEP or abstract for papers
   authors: list[str]
   editors: list[str]
   original_date: str
@@ -14,26 +15,24 @@ class Metadata(TypedDict):
   bibliography: list[str]
   
 class VectorMetadata(Metadata):
+  title: str
+  headers: list[str]
   num_tokens: int
   
 class Article(TypedDict):
   id: str
   title: str
-  # metadata: Metadata # TODO: make this work - have to refactor scraper, etc. and rerun
-  authors: list[str]
-  editors: list[str]
-  original_date: str
-  revision_date: str | None
-  link: str
   content: list[Section]
-  bibliography: list[str]
+  metadata: Metadata
 
 class Chunk(TypedDict):
   id: str
   num_tokens: int
-  content: str # must be under 1.95k tokens
-  title: str # must be 0.05k - 0.98k tokens
   text_metadata: Metadata
+  content: str # must be under 1.95k tokens
+  # must be 0.05k - 0.98k tokens
+  title: str
+  headers: list[str] 
   
 # makes it easy for Pinecone ingestion
 class Embedding(TypedDict):
