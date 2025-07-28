baabce8a94a0b29a72d47e32a363c29f278d19c6
akshayirudayaraj <akshay.irudayaraj@gmail.com>
2025-07-27 19:46:23 -0700

    scraping abstractions/refactoring (untested)

---
+++ sep/scraper.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from collections.abc import Generator
+import requests
+from bs4 import BeautifulSoup, Tag
+from json_helper import JsonHelper
+from shared_types import Article
+
+class SkipIteration(Exception):
+  pass
+
+class Scraper(ABC):
+  _base_scraping_url: str
+  _beautiful_soup: BeautifulSoup
+  
+  def __init__(self, base_scraping_url: str):
+    self._base_scraping_url = base_scraping_url
+    self._beautiful_soup = self._fetch_url(self._base_scraping_url)
+  
+  def _fetch_url(self, link: str) -> BeautifulSoup:
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, "lxml")
+    return soup
+  
+  def find_required(self, soup: BeautifulSoup, **kwargs) -> Tag:
+    tag = soup.find(**kwargs)
+    if not isinstance(tag, Tag):
+      raise ValueError("unable to find tag! double check your search criteria or the raw html in the document.")
+    else:
+      return tag
+      
+  # might be specifying way too much here, in which case i'll generalize as i add more data sources
+      
+  @abstractmethod
+  def extract_links(self) -> Generator[str]:
+    pass
+  
+  @abstractmethod
+  def scrape_article(self, link: str) -> Article:
+    print(f'scraping {link}')
+    pass
+  
+  def scrape_articles(self) -> Generator[Article]:
+    links = self.extract_links()
+    for link in links:
+      try:
+        yield self.scrape_article(link)
+      except SkipIteration:
+        pass
+  
+  def scrape_and_store(self, base_write_directory: str) -> None:
+    articles = self.scrape_articles()
+    for idx, article in enumerate(articles, 1):
+      JsonHelper.write_dict_to_json(dict=article, filepath=base_write_directory)
+      print(f'Wrote {article} to {base_write_directory}, #{idx}')
--- sep/sep_scraping.py
+++ sep/sep_scraping.py
@@ -1,209 +1,192 @@
-'''
-  Currently doesn't work after some code refactoring
-  
-  TODO:
-    - Modify Article object construction to use the Metadata instance variable
-    - All JSON handling should come from the JsonHelper class
-'''
-
-from typing import cast
-import requests
-from bs4 import BeautifulSoup, Tag
-
-import json
+from collections.abc import Generator
 import os
 import datetime
 import string
+from typing import cast
+
+from bs4 import Tag
+from scraper import Scraper, SkipIteration
 from shared_types import Article, Section
 
 from pylatexenc.latex2text import LatexNodes2Text
 
-def fetch_url(link: str) -> BeautifulSoup:
-  page = requests.get(link)
-  soup = BeautifulSoup(page.content, "lxml")
-  return soup
+class SepScraper(Scraper):
+  def get_dates(self, date_info: str) -> dict[str, str | None]:
+    DATE_FORMAT = '%a %b %d, %Y'
 
-def get_dates(date_info: str, date_format: str) -> tuple[str, str | None]:
     date_parts = date_info.split(';')
     
     original_date = date_parts[0]
-    original_date = datetime.datetime.strptime(original_date.removeprefix("First published "), date_format)
+    original_date = datetime.datetime.strptime(original_date.removeprefix("First published "), DATE_FORMAT)
     original_date = original_date.isoformat()
     
     revision_date = date_parts[1] if len(date_parts) > 1 else None
     if revision_date:
-      revision_date = datetime.datetime.strptime(revision_date.removeprefix(" substantive revision "), date_format)
+      revision_date = datetime.datetime.strptime(revision_date.removeprefix(" substantive revision "), DATE_FORMAT)
       revision_date = revision_date.isoformat()
       
-    return original_date, revision_date
-  
-def get_content_by_section(all_tags) -> list[Section]:
-  section_contents = []
-  running_header_nest_tree = []
-  header_tags = ['h1', 'h2', 'h3', 'h4']
+    return {
+      'original_date': original_date,
+      'revision_date': revision_date
+    }
     
-  for i in all_tags:
-    if i.name in header_tags:
-      # keep a running memory and prefix that running header history to the section metadata
-      # lower values take out last value in running list, higher values append to running list
+  def get_content_by_section(self, all_tags) -> list[Section]:
+    section_contents = []
+    running_header_nest_tree = []
+    header_tags = ['h1', 'h2', 'h3', 'h4']
       
-      # TODO: delete entries where this is a header that has its own text (why not empty?)
-        # assumption right now: every header has some (valuable) paragraph text underneath it before the next header,
-        # which isn't necessarily true
-      
-      # print(running_header_nest_tree)
-      
-      prev = i.find_previous(header_tags)
-      
-      curr_header_text = i.get_text().lstrip(string.digits + '.' + ' ')
-      
-      # TODO: make custom comparator for code clarity if i really wanna
-      curr_header_value = int(i.name.removeprefix('h'))
-      prev_header_value = int(prev.name.removeprefix('h')) if prev is not None else None
-      
-      # matches entry # in list to the header #
-        # empty h2 entry in case of h1->h3 (people make mistakes)
-      if prev_header_value is None:
-        pass
-      elif curr_header_value <= prev_header_value:
-        while curr_header_value <= prev_header_value:
-          running_header_nest_tree.pop()
-          prev_header_value -= 1
-      else:
-        while curr_header_value > prev_header_value+1:
-          running_header_nest_tree.append("")
-          curr_header_value -= 1
-          
-      running_header_nest_tree.append(curr_header_text)
-          
-      section_contents.append({
-        "header": running_header_nest_tree.copy(),
-        "text": "",
-      })
+    for i in all_tags:
+      if i.name in header_tags:
+        # keep a running memory and prefix that running header history to the section metadata
+        # lower values take out last value in running list, higher values append to running list
         
-    else:
-      if section_contents: # should always exist (headers always first) so this is for sanity
-        section_contents[-1]["text"] += LatexNodes2Text().latex_to_text(i.get_text()).replace("\n", " ")
+        # TODO: delete entries where this is a header that has its own text (why not empty?)
+          # assumption right now: every header has some (valuable) paragraph text underneath it before the next header,
+          # which isn't necessarily true
+        
+        # print(running_header_nest_tree)
+        
+        prev = i.find_previous(header_tags)
+        
+        curr_header_text = i.get_text().lstrip(string.digits + '.' + ' ')
+        
+        # TODO: make custom comparator for code clarity if i really wanna
+        curr_header_value = int(i.name.removeprefix('h'))
+        prev_header_value = int(prev.name.removeprefix('h')) if prev is not None else None
+        
+        # matches entry # in list to the header #
+          # empty h2 entry in case of h1->h3 (people make mistakes)
+        if prev_header_value is None:
+          pass
+        elif curr_header_value <= prev_header_value:
+          while curr_header_value <= prev_header_value:
+            running_header_nest_tree.pop()
+            prev_header_value -= 1
+        else:
+          while curr_header_value > prev_header_value+1:
+            running_header_nest_tree.append("")
+            curr_header_value -= 1
+            
+        running_header_nest_tree.append(curr_header_text)
+            
+        section_contents.append({
+          "header": running_header_nest_tree.copy(),
+          "text": "",
+        })
+          
+      else:
+        if section_contents: # should always exist (headers always first) so this is for sanity
+          section_contents[-1]["text"] += LatexNodes2Text().latex_to_text(i.get_text()).replace("\n", " ")
 
-  '''
-    latex conversion to natural language: https://pylatexenc.readthedocs.io/en/latest/latex2text/
-      certain logic operators aren't handled like logical and/or/not (\\land, etc.) - satisfactory for now
-      because some people use the synonymous \\wedge, etc.
-      
-      might want to find better conversion in the future
-      
-    how to handle logic formula trees??
-  '''
-  
-  return section_contents
-  
-class SkipIteration(Exception):
-  pass
-
-def get_author_information(article_id: str) -> tuple[list[str], list[str]]:
-  soup = fetch_url(f'https://plato.stanford.edu/cgi-bin/encyclopedia/archinfo.cgi?entry={article_id}')
-  
-  citation_tag = soup.find('pre')
-  assert citation_tag is not None, "The citation link being looked at is seriously messed up"
-  citation = citation_tag.get_text()
-  
-  segments = citation.removeprefix(f'@InCollection{{sep-{article_id},').split('},')
-  
-  authors_combined = segments[0].split('{')[-1]
-  editors_combined = segments[3].split('{')[-1]
-  
-  authors_last_first = authors_combined.split(' and ')
-  editors_full_name = editors_combined.split(' and ')
-  
-  authors_full_name = [" ".join(author.split(', ')[::-1]) for author in authors_last_first]
-  
-  return authors_full_name, editors_full_name
-
-def find_required(soup, **kwargs) -> Tag:
-  tag = soup.find(**kwargs)
-  if not isinstance(tag, Tag):
-    raise ValueError("Unable to find tag! Something's wrong with the document you're searching.")
-  else:
-    return tag
-
-def extract_article_data(link: str) -> Article:
-  soup = fetch_url(link)
-
-  title = find_required(soup, name="h1").get_text()
-  if (title == 'Document Retired'):
-    raise SkipIteration()
-  
-  date_format = '%a %b %d, %Y'
-  date_info = find_required(soup, id="pubinfo").get_text()
-  original_date, revision_date = get_dates(date_info, date_format)
-  
-  preamble = find_required(soup, id="preamble").get_text()
-  
-  main_content = find_required(soup, id="main-text")
-  all_tags = main_content.descendants # includes nested
-  
-  # TODO: clean up some duplicate phrases in all_tags
-  
-  section_contents = get_content_by_section(all_tags)
-  
-  # TODO: move title, preamble/summary to metadata
-  section_contents.append({
-    "header": list(title),
-    "text": preamble,
-  }) # this technically should come first, but order shouldn't matter here and this allows for cleaner code
-  
-  biblio_list = [entry.get_text().replace("\n", " ") for entry in find_required(soup, id="bibliography").find_all("li")]
-  
-  authors, editors = get_author_information(link.split('/')[-2])
-  
-  return {
-    'id': "sep-" + title.lower().replace(' ', '-').replace('/', '-'),
-    'title': title,
-    'authors': authors,
-    'editors': editors,
-    'original_date': original_date,
-    'revision_date': revision_date,
-    'link': link,
-    'content': section_contents,
-    'bibliography': biblio_list,
-  }
-  
-def get_all_post_links(soup: BeautifulSoup) -> list[str]:
-  article_entries = find_required(soup, id="content").find_all("a")
-  links = [entry['href'] for entry in article_entries] # type: ignore
-  # print(*links, sep='\n')
-  return links # type: ignore
-
-def write_dict_to_json(dict: dict, filepath: str) -> None:
-  with open(filepath + '.json', 'w') as file:
-    json.dump(dict, file, indent=4, ensure_ascii=False)
-      
-def scrape_article_and_store(link: str):
-  print("scraping " + link)
+    '''
+      latex conversion to natural language: https://pylatexenc.readthedocs.io/en/latest/latex2text/
+        certain logic operators aren't handled like logical and/or/not (\\land, etc.) - satisfactory for now
+        because some people use the synonymous \\wedge, etc.
+        
+        might want to find better conversion in the future
+        
+      how to handle logic formula trees??
+    '''
     
-  article = extract_article_data(link)
-  
-  print("writing " + article['title'])
-  
-  filepath = os.path.join(os.getcwd(), 'sep', 'articles', article['id'])
-  
-  write_dict_to_json(cast(dict, article), filepath)
+    return section_contents
+
+  def get_contributor_information(self, article_id: str) -> dict[str, list[str]]:
+    soup = self._fetch_url(f'https://plato.stanford.edu/cgi-bin/encyclopedia/archinfo.cgi?entry={article_id}')
     
-  print("finished writing " + article['title'] + "!\n")
+    citation_tag = soup.find('pre')
+    assert citation_tag is not None, "the citation link being looked at is seriously messed up"
+    citation = citation_tag.get_text()
+    
+    segments = citation.removeprefix(f'@InCollection{{sep-{article_id},').split('},')
+    
+    authors_combined = segments[0].split('{')[-1]
+    editors_combined = segments[3].split('{')[-1]
+    
+    authors_last_first = authors_combined.split(' and ')
+    editors_full_name = editors_combined.split(' and ')
+    
+    authors_full_name = [" ".join(author.split(', ')[::-1]) for author in authors_last_first]
+    
+    return {
+      'authors': authors_full_name,
+      'editors': editors_full_name
+    }
+
+  def scrape_article(self, link: str) -> Article:
+    soup = self._fetch_url(link)
+
+    title = self.find_required(soup, name="h1").get_text()
+    if (title == 'Document Retired'):
+      raise SkipIteration()
+    
+    date_info = self.find_required(soup, id="pubinfo").get_text()
+    dates = self.get_dates(date_info)
+    
+    preamble = self.find_required(soup, id="preamble").get_text()
+    
+    main_content = self.find_required(soup, id="main-text")
+    all_tags = main_content.descendants # includes nested
+    
+    # TODO: clean up some duplicate phrases in all_tags
+    
+    section_contents = self.get_content_by_section(all_tags)
+    
+    biblio_list = [entry.get_text().replace("\n", " ") for entry in self.find_required(soup, id="bibliography").find_all("li")]
+    
+    sep_url_article_id = link.split('/')[-2]
+    contributors = self.get_contributor_information(sep_url_article_id)
+    
+    return {
+      'id': "sep-" + title.lower().replace(' ', '-').replace('/', '-'),
+      'title': title,
+      'content': section_contents,
+      'metadata': {
+        'authors': contributors['authors'],
+        'editors': contributors['editors'],
+        'original_date': cast(str, dates['original_date']), # hacky solution (i know original_date will always exist)
+        'revised_date': dates['revision_date'],
+        'link': link,
+        'bibliography': biblio_list,
+        'intro': preamble,
+        'organization': 'Stanford Encyclopedia of Philosophy'
+      },
+    }
+    
+  def extract_links(self) -> Generator[str]:
+    article_entries = self.find_required(self._beautiful_soup, id="content").find_all("a")
+    for entry in article_entries:
+      safe_entry = cast(Tag, entry) # lots of casts, too lazy rn to work around
+      link = safe_entry.get('href')
+      yield cast(str, link)
+      
+# def scrape_article_and_store(link: str):
+#   print("scraping " + link)
+    
+#   article = extract_article_data(link)
+  
+#   print("writing " + article['title'])
+  
+#   filepath = os.path.join(os.getcwd(), 'sep', 'articles', article['id'])
+  
+#   JsonHelper.write_dict_to_json(cast(dict, article), filepath)
+    
+#   print("finished writing " + article['title'] + "!\n")
 
 def main():
   sep_chronological_entries = "https://plato.stanford.edu/published.html"
-  main_soup = fetch_url(sep_chronological_entries)
-
-  entries = get_all_post_links(main_soup)
+  sep_scraper = SepScraper(base_scraping_url=sep_chronological_entries)
   
-  for i, link in enumerate(entries, 1):
-    try:
-      scrape_article_and_store(link)
-    except SkipIteration:
-      pass
+  article_directory = os.path.join('sep', 'articles')
+  sep_scraper.scrape_and_store(base_write_directory=article_directory)
+  # entries = get_all_post_links(main_soup)
+  
+  # for i, link in enumerate(entries, 1):
+  #   try:
+  #     scrape_article_and_store(link)
+  #   except SkipIteration:
+  #     pass
     
-    print(f'{i}/1852')
+  #   print(f'{i}/1852')
   
 if __name__ == '__main__':
   main()
