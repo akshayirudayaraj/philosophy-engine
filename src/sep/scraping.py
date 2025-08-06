fe01269ec180905c9f4513036953ae540778e94e
akshayirudayaraj <akshay.irudayaraj@gmail.com>
2025-08-05 17:20:40 -0700

    fix scraper scraping lots of duplicate text, add more abstraction & exception handling

--- src/sep/scraping.py
+++ src/sep/scraping.py
@@ -1,32 +1,36 @@
 from collections.abc import Generator
-import os
-import datetime
 import string
 from typing import cast
 
-from core.storage.sqlite_helper import SqliteHelper
 from bs4 import Tag
-from core.scraping.scraper import _BaseScraper, SkipIteration
+import datetime
+from pylatexenc.latex2text import LatexNodes2Text
+import json
+
+from core.storage.sqlite_helper import SqliteHelper, ArticleStorage
+from core.scraping.scraper import _BaseScraper, SkipIteration, ParseException
 from shared_types import Article, Section
 
-from pylatexenc.latex2text import LatexNodes2Text
-
+# TODO: refactor class so it's more modular and functions aren't so tall
 class SepScraper(_BaseScraper):
+  DATE_FORMAT: str = '%a %b %d, %Y'
+  _ACCEPTED_HEADER_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+  _ACCEPTED_TEXT_TAGS = ['p', 'blockquote', 'ol', 'ul', 'div']
+  
   def __init__(self, base_scraping_url: str):
     super().__init__(base_scraping_url)
   
-  def get_dates(self, date_info: str) -> dict[str, str | None]:
-    DATE_FORMAT = '%a %b %d, %Y'
+  def _get_dates(self, date_info: str) -> dict[str, str | None]:
 
     date_parts = date_info.split(';')
     
     original_date = date_parts[0]
-    original_date = datetime.datetime.strptime(original_date.removeprefix("First published "), DATE_FORMAT)
+    original_date = datetime.datetime.strptime(original_date.removeprefix("First published "), self.DATE_FORMAT)
     original_date = original_date.isoformat()
     
     revision_date = date_parts[1] if len(date_parts) > 1 else None
     if revision_date:
-      revision_date = datetime.datetime.strptime(revision_date.removeprefix(" substantive revision "), DATE_FORMAT)
+      revision_date = datetime.datetime.strptime(revision_date.removeprefix(" substantive revision "), self.DATE_FORMAT)
       revision_date = revision_date.isoformat()
       
     return {
@@ -34,53 +38,79 @@ class SepScraper(_BaseScraper):
       'revision_date': revision_date
     }
     
-  def get_content_by_section(self, all_tags) -> list[Section]:
-    section_contents = []
-    running_header_nest_tree = []
-    header_tags = ['h1', 'h2', 'h3', 'h4']
-      
-    for i in all_tags:
-      if i.name in header_tags:
-        # keep a running memory and prefix that running header history to the section metadata
-        # lower values take out last value in running list, higher values append to running list
+  def _update_running_header_list(self, current_element: Tag, prev_header: Tag | None, running_header_nest_list: list[str]) -> None:
+    """
+    keep a running memory and prefix that running header history to the section metadata\n
+    lower values take out last value in running list, higher values append to running list\n
+    matches entry # in list to the header #\n
+    - empty h2 entry in case of h1->h3 (people make mistakes)
+    """
         
-        # TODO: delete entries where this is a header that has its own text (why not empty?)
-          # assumption right now: every header has some (valuable) paragraph text underneath it before the next header,
-          # which isn't necessarily true
-        
-        # print(running_header_nest_tree)
-        
-        prev = i.find_previous(header_tags)
-        
-        curr_header_text = i.get_text().lstrip(string.digits + '.' + ' ')
-        
-        # TODO: make custom comparator for code clarity if i really wanna
-        curr_header_value = int(i.name.removeprefix('h'))
-        prev_header_value = int(prev.name.removeprefix('h')) if prev is not None else None
-        
-        # matches entry # in list to the header #
-          # empty h2 entry in case of h1->h3 (people make mistakes)
-        if prev_header_value is None:
-          pass
-        elif curr_header_value <= prev_header_value:
-          while curr_header_value <= prev_header_value:
-            running_header_nest_tree.pop()
-            prev_header_value -= 1
-        else:
-          while curr_header_value > prev_header_value+1:
-            running_header_nest_tree.append("")
-            curr_header_value -= 1
-            
-        running_header_nest_tree.append(curr_header_text)
-            
-        section_contents.append({
-          "header": running_header_nest_tree.copy(),
-          "text": "",
-        })
-          
+    if prev_header is not None:      
+      curr_header_value = int(current_element.name.removeprefix('h'))
+      prev_header_value = int(prev_header.name.removeprefix('h'))
+
+      if current_element.name <= prev_header.name:
+        while curr_header_value <= prev_header_value:
+          running_header_nest_list.pop()
+          prev_header_value -= 1
       else:
-        if section_contents: # should always exist (headers always first) so this is for sanity
-          section_contents[-1]["text"] += LatexNodes2Text().latex_to_text(i.get_text()).replace("\n", " ")
+        while curr_header_value > prev_header_value+1:
+          running_header_nest_list.append("")
+          curr_header_value -= 1
+    
+    curr_header_text = current_element.get_text().lstrip(string.digits + '.' + ' ')
+    running_header_nest_list.append(curr_header_text)
+    
+  def _extract_content_by_section(self, main_content: Tag) -> list[Section]:
+    def add_content_to_last_section(text: str) -> None:
+      # text = text.strip()
+      text = LatexNodes2Text().latex_to_text(text)
+      text = text.replace("\n", " ")
+      section_contents[-1]['text'] += text
+    
+    content_children = main_content.children # tags and loose text, but no duplicates (as opposed to descendants)
+      # PageElement is an abstract class (non exported), so it's important that this is here for type checking
+      # each PageElement (what this iterates over) is either a Tag or NavigableString
+        
+    section_contents = []
+    running_header_nest_list = []
+      
+    for current_element in content_children:
+      if not isinstance(current_element, Tag): # then: NavigableString
+        current_element = str(current_element)
+
+        if current_element.strip() == '': # Empty string element; these happen often due to the way SEP formats HTML
+          continue
+
+        add_content_to_last_section(current_element)
+      
+      elif current_element.name in self._ACCEPTED_HEADER_TAGS:
+          prev_child = current_element.find_previous(name=self._ACCEPTED_HEADER_TAGS)
+          
+          if not isinstance(prev_child, Tag | None):
+            raise ParseException('prev_header current_element not a Tag or None')
+            # there's no way this is an object other than a Tag because we're finding previous based on tag name
+          
+          self._update_running_header_list(current_element, prev_child, running_header_nest_list)
+          
+          if section_contents: # check this isn't first iteration
+            # idea is that we do some trimming/cleaning operations when we know we're adding a new item to the list]
+            if section_contents[-1]['text'] == "":
+              section_contents.pop()
+            else:
+              section_contents[-1]['text'] = section_contents[-1]['text'].strip()
+
+          section_contents.append({
+            "header": running_header_nest_list.copy(),
+            "text": "",
+          })
+          
+      elif current_element.name in self._ACCEPTED_TEXT_TAGS:
+        add_content_to_last_section(current_element.get_text())
+        
+      else:
+        raise ParseException(f"Weird tag {current_element.name} unable to be processed. Direct current_element of main-text")
 
     '''
       latex conversion to natural language: https://pylatexenc.readthedocs.io/en/latest/latex2text/
@@ -89,16 +119,19 @@ class SepScraper(_BaseScraper):
         
         might want to find better conversion in the future
         
-      how to handle logic formula trees??
+      TODO: how to handle logic formula lists??
     '''
     
     return section_contents
 
-  def get_contributor_information(self, article_id: str) -> dict[str, list[str]]:
+  def _get_contributor_information(self, article_id: str) -> dict[str, list[str]]:
     soup = self._fetch_url(f'https://plato.stanford.edu/cgi-bin/encyclopedia/archinfo.cgi?entry={article_id}')
     
     citation_tag = soup.find('pre')
-    assert citation_tag is not None, "the citation link being looked at is seriously messed up"
+    
+    if not isinstance(citation_tag, Tag):
+      raise ParseException("The <pre> tag for getting citations doesn't exist")
+    
     citation = citation_tag.get_text()
     
     segments = citation.removeprefix(f'@InCollection{{sep-{article_id},').split('},')
@@ -121,24 +154,21 @@ class SepScraper(_BaseScraper):
 
     title = self.find_required(article_soup, name="h1").get_text()
     if (title == 'Document Retired'):
-      raise SkipIteration()
+      raise SkipIteration(title)
     
     date_info = self.find_required(article_soup, id="pubinfo").get_text()
-    dates = self.get_dates(date_info)
+    dates = self._get_dates(date_info)
     
-    preamble = self.find_required(article_soup, id="preamble").get_text()
+    raw_preamble = self.find_required(article_soup, id="preamble").get_text()
+    preamble = raw_preamble.replace("\n", " ").strip()
     
-    main_content = self.find_required(article_soup, id="main-text")
-    all_tags = main_content.descendants # includes nested
-    
-    # TODO: clean up some duplicate phrases in all_tags
-    
-    section_contents = self.get_content_by_section(all_tags)
+    main_content = self.find_required(article_soup, id="main-text")    
+    section_contents = self._extract_content_by_section(main_content)
     
     biblio_list = [entry.get_text().replace("\n", " ") for entry in self.find_required(article_soup, id="bibliography").find_all("li")]
     
     sep_url_article_id = link.split('/')[-2]
-    contributors = self.get_contributor_information(sep_url_article_id)
+    contributors = self._get_contributor_information(sep_url_article_id)
     authors = contributors['authors']
     editors = contributors['editors']
     
@@ -149,7 +179,7 @@ class SepScraper(_BaseScraper):
       'metadata': {
         'authors': authors,
         'editors': editors,
-        'original_date_published': cast(str, dates['original_date']), # hacky solution (i know original_date will always exist)
+        'original_date_published': cast(str, dates['original_date']), # hacky solution (current_element know original_date will always exist)
         'revision_date': dates['revision_date'],
         'link': link,
         'bibliography': biblio_list,
@@ -160,97 +190,28 @@ class SepScraper(_BaseScraper):
     
   def extract_links(self) -> Generator[str]:
     article_entries = self.find_required(id="content").find_all("a")
-    for entry in article_entries:
-      safe_entry = cast(Tag, entry) # lots of casts, too lazy rn to work around
-      link = safe_entry.get('href')
-      yield cast(str, link)
+    for link_element in article_entries:
+      if not isinstance(link_element, Tag):
+        raise ParseException('A random NavigableString has made its way into the list of article entries')
       
-# def scrape_article_and_store(link: str):
-#   print("scraping " + link)
-    
-#   article = extract_article_data(link)
-  
-#   print("writing " + article['title'])
-  
-#   filepath = os.path.join(os.getcwd(), 'sep', 'articles', article['id'])
-  
-#   JsonHelper.write_dict_to_json(cast(dict, article), filepath)
-    
-#   print("finished writing " + article['title'] + "!\n")
-
-# SEP data structure:
-# article_metadata -> stores id, title, and other metadata
-# article_content -> stores id, header_tree_name, text_associated, 
-  # where there are multiple entries for each id since there are multiple sections in each article
-
-def format_article_for_db(datum: Article) -> dict:
-  return {
-      'id': datum['id'],
-      'title': datum['title'],
-      'organization': datum['metadata']['organization'],
-      'intro': datum['metadata']['intro'],
-      'authors': ",".join(datum['metadata']['authors']),
-      'editors': ",".join(datum['metadata']['editors']),
-      'original_date': datum['metadata']['original_date_published'],
-      'revised_date': datum['metadata']['revision_date'],
-      'link': datum['metadata']['link'],
-      'bibliography': '\n'.join(datum['metadata']['bibliography'])
-  }
+      link = link_element.get('href')
+      yield cast(str, link)
 
 def main():
   sep_chronological_entries = "https://plato.stanford.edu/published.html"
   sep_scraper = SepScraper(base_scraping_url=sep_chronological_entries)
   
-  # TODO: abstract db creation and handling (all of the below) into articledb class that uses sqlite helper
-  db = SqliteHelper('sep.db')
+  sqlite_db_interface = SqliteHelper('sep.db')
+  article_storage_helper = ArticleStorage(sqlite_db_interface)
   
-  db.create_table_if_not_created(
-    "article_metadata",
-    "id TEXT PRIMARY KEY",
-    "title TEXT NOT NULL",
-    "organization TEXT NOT NULL",
-    "intro TEXT",
-    "authors TEXT NOT NULL",
-    "editors TEXT",
-    "original_date_published TEXT NOT NULL",
-    "date_revised TEXT",
-    "link TEXT NOT NULL",
-    "bibliography TEXT"
-  )
-  
-  db.create_table_if_not_created(
-    "article_content",
-    "section_id TEXT PRIMARY KEY",
-    "associated_article_id INTEGER NOT NULL",
-    "header_list TEXT NOT NULL",
-    "content TEXT NOT NULL",
-    "FOREIGN KEY (associated_article_id) REFERENCES article_metadata(id) ON DELETE CASCADE"
-  )
+  article_storage_helper.create_article_content_and_metadata_tables()
   
   data = sep_scraper.scrape_articles()
   
-  for datum in data:
-    formatted_datum = format_article_for_db(datum)
-    
-    db.execute_change("""
-                      INSERT OR REPLACE 
-                      INTO article_metadata
-                      VALUES (
-                        :id, :title, :organization, :intro, :authors, :editors, :original_date,
-                        :revised_date, :link, :bibliography
-                      );
-                      """, formatted_datum)
-    
-    for section in datum['content']:
-      deepest_header = section['header'][-1]
-      section_id = formatted_datum['id'] + '-' + deepest_header.lower().replace(' ', '-').replace('/', '-')
-      header_list = ",".join(section['header'])
-      
-      db.execute_change("""
-                        INSERT OR REPLACE
-                        INTO article_content
-                        VALUES (?, ?, ?, ?);
-                        """, (section_id, formatted_datum['id'], header_list, section['text']))
+  article_storage_helper.store_article_dictionaries(data)
+  
+  # data = sep_scraper.scrape_article('https://plato.stanford.edu/entries/logic-games/')
+  # print(json.dumps(data['content'], ensure_ascii=False, indent=4))
   
 if __name__ == '__main__':
   main()
