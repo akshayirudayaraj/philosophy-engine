96ca29e4ee250f06152a7a84105099ed99640e45
akshayirudayaraj <akshay.irudayaraj@gmail.com>
2025-08-08 14:25:46 -0700

    replace unbound function with bound function for vector processing before upsertion

--- src/sep/pinecone_storage.py
+++ src/sep/pinecone_storage.py
@@ -8,25 +8,8 @@ def main():
   read_directory = os.path.join('data', 'sep_v2', 'embeddings')
   pinecone = PineconeDB.from_environment()
   
-  vectors = JsonHelper.process_files(read_directory=read_directory, fn=PineconeDB.process_and_clean_vector)
+  vectors = JsonHelper.process_files(read_directory=read_directory, fn=pinecone.process_and_clean_vector)
   pinecone.upsert_all_vectors(cast(Generator[Vector], vectors), namespace="better_article_data") # FIXME: type checker confusion about Vector & dict
-  
-  # pc = Pinecone(PINECONE_API_KEY)
-  # index = pc.Index(host=PC_INDEX, 
-  #                  pool_threads=10)
-    
-  # with index: # generators are so cool!
-  #   vectors = process_vectors()
-  #   batches = fast_chunker(vectors)
-    
-  #   for batch in batches:
-      # futures = [index.upsert(cast(list[Vector], batch), async_req=True, show_progress=True)]
-      
-      # for future in futures: # using ignores here bc i'm following docs (future.get() should raise on error)
-      #   try:
-      #     future.get() # type: ignore
-      #   except Exception:
-      #     logging.error(f"Error upserting {future.get()}") # type: ignore
   
 if __name__ == '__main__':
   main()
