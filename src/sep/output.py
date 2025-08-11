7f9374bc62d1b965ec69cacbfe30000f0a85c197
akshayirudayaraj <akshay.irudayaraj@gmail.com>
2025-08-10 23:07:25 -0700

    increased context (hitting ~25-28k input tokens now), increased max output length

--- src/sep/output.py
+++ src/sep/output.py
@@ -3,18 +3,18 @@ from core.embedding.gemini_embedder import GeminiEmbedder
 from core.prompting.prompt_handler import PromptHandlerFactory, LargeLanguageModels
 
 def main():
-  user_query = "To what extent, if any, can a machine be considered conscious?"
-  
+  user_query = "To what extent, if any, does free will exist?"
+
   gemini_embedder = GeminiEmbedder(output_dim=1536, task_type='RETRIEVAL_QUERY')
   query_vector = gemini_embedder.embed(user_query)
   
   pinecone = PineconeDB.from_environment()
   
-  result = pinecone.query(query_vector, num_res_to_retrieve=10, namespace='better_article_data')    
+  result = pinecone.query(query_vector, num_res_to_retrieve=12, namespace='better_article_data')    
   top_k_vectors = result['matches']
   print(f'usage: {result['usage']}')
   
-  prompt_handler = PromptHandlerFactory.create_prompt_handler(LargeLanguageModels.GPT_5, max_response_words=5000)
+  prompt_handler = PromptHandlerFactory.create_prompt_handler(LargeLanguageModels.GPT_5, max_response_words=5500)
   prompt = prompt_handler.construct_prompt(user_query, top_k_vectors)
   prompt_handler.prompt_model(prompt)
   
